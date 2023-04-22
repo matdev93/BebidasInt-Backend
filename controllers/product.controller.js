5e551e644e8621168412840f3bfba44a93da85d8
matdev93 <112671782+matdev93@users.noreply.github.com>
2023-04-22 01:54:19 -0400

    ajustes y reduce stock

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -72,10 +72,24 @@ const deleteProduct = async (req, res) => {
   }
 }
 
+const reduceStock = async(req, res) => {
+  const productosComprado = req.body.cartItems;
+  try {
+      productosComprado.map(async(producto) => {
+          await Product.findByIdAndUpdate(producto._id, {stock: producto.stock - producto.quantity})
+      })
+  res.json({msg: "Se ha reducido el stock de los productos", success: true})
+  } catch (error) {
+      res.status(500).json({success: false, message: error.message})
+  }
+}
+
+
 module.exports = {
   getProducts,
   createProduct,
   getProductById,
   editProduct,
   deleteProduct,
+  reduceStock
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 
-const { getProducts, createProduct, getProductById, editProduct, deleteProduct } = require('../controllers/product.controller');
+const { getProducts, createProduct, getProductById, editProduct, deleteProduct, reduceStock  } = require('../controllers/product.controller');
 
 const productRouter = express.Router();
 
@@ -13,5 +13,8 @@ productRouter
   .route('/products/:productId')
   .put(editProduct)
   .delete(deleteProduct);
+
+productRouter.route("/product/reduce-stock")
+  .put(reduceStock)
 
 module.exports = productRouter;
